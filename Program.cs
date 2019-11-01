e293681563e915c755db6dbf959e71fd4fb1c879
Floralforest19 <56820062+Floralforest19@users.noreply.github.com>
2019-11-01 09:41:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tarningssimulatorn
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många tärningar vill du kasta?");
+        }
+    }
+}
